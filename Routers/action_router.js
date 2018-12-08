098bf7670e3b348e34012948d8ed96b61e93e429
Kendra Williams <kmonique.williams@gmail.com>
2018-12-08 14:36:29 -0500

    action_router completed

--- Routers/action_router.js
+++ Routers/action_router.js
@@ -26,8 +26,51 @@ router.get("/:id", (req, res) => {
 //add action
 router.post("/", (req, res) => {
    const action = req.body;
-   if(action.project_Id && action.description && action.notes) {
+   if(action.project_id && action.description && action.notes) {
+      if(action.description.length < 128) {
+         actionDb.insert(action)
+            .then(action => {
+               res.status(201).json(action);
+            })
+            .catch(err => {
+               res.status(500).json({error: "error adding action"});
+            })
+      } else {
+         res.status(400).json({error: "action description must be 128 characters or less"});
+      }
+   } else {
+      res.status(400).json({error: "please provide the description, notes and project id for the action"});
+   }
+});
 
+//delete action
+router.delete("/:id", (req, res) => {
+   const {id} = req.params;
+   actionDb.remove(id)
+   .then(count => {
+      count ? res.json({message: "action deleted successfully"}) : res.status(404).json({error: "action does not exist"});
+   })
+   .catch(err => { res.status(500).json({error: "error removing project"}); });
+});
+
+//update action
+router.put("/:id", (req, res) => {
+   const {id} = req.params;
+   const action = req.body;
+   if(action.project_id && action.description && action.notes) {
+      if(action.description.length < 128) {
+         actionDb.update(id, action)
+            .then(action => {
+               action != null ? res.json(action) : res.status(404).json({error: "action not found"})
+            })
+            .catch(err => {
+               res.status(500).json({error: "error updating action"});
+            })
+      } else {
+         res.status(400).json({error: "action description must be 128 characters or less"});
+      }
+   } else {
+      res.status(400).json({error: "please provide the description, notes and project id for the action"});
    }
 });
 
